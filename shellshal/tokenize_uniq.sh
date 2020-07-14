c0195ffe9598eca0a626b3b857046ed6d4476b6f
linuxscout <taha_zerrouki@hotmail.com>
2020-07-14 18:57:24 +0100

    strip extrat spaces

--- shellshal/tokenize_uniq.sh
+++ shellshal/tokenize_uniq.sh
@@ -10,5 +10,5 @@
 # Tokenize words from text and return only sorted uniq words with frequency
 #   tokenize_uniq.sh filename
 #~ tr -s '[*!"#\$%&\(\)\+,\\\.\/:;،؛<=>\?@\[\\\\]^_`\{|\}~][:space:]]' '\n' < $1 | sort | uniq -c | sort -nr 
-sed 's/[[:punct:][:space:]×،؛]/\n/g'  < $1 |sed '/^\s*$/d' | sort | uniq -c | sort -nr 
+sed 's/[[:punct:][:space:]×،؛]/\n/g'  < $1 |sed '/^\s*$/d' | sort | uniq -c | sort -nr |sed "s/^[ ]*//g"
 
